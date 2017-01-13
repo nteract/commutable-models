d9bfa1c9f7b0b0ddf31f9dd75d3ea697d36130af
Kyle Kelley <rgbkrk@gmail.com>
2017-01-13 11:55:51 -0800

    just prior to conversion

--- src/models.js
+++ src/models.js
@@ -1,3 +1,6 @@
+import * as Immutable from 'immutable'; // This is required
+import ImmutableModel from 'flow-immutable-models'; // Make sure you copied this file into your repo
+
 export type ImmutableJSON = | string | number | boolean | null
                             | ImmutableJSONMap | ImmutableJSONList; // eslint-disable-line no-use-before-define
 export type ImmutableJSONMap = Immutable.Map<string, ImmutableJSON>;
@@ -7,56 +10,56 @@ export type ExecutionCount = number | null;
 
 export type MimeBundle = Immutable.Map<string, ImmutableJSON>;
 
-export type ExecuteResult = {
+export type ExecuteResultModelType = {
   output_type: 'execute_result',
   execution_count: ExecutionCount,
   data: MimeBundle,
   metadata: ImmutableJSONMap,
 }
 
-export type DisplayData = {
+export type DisplayDataModelType = {
   output_type: 'display_data',
   data: MimeBundle,
   metadata: ImmutableJSONMap,
 }
 
-export type StreamOutput = {
+export type StreamOutputModelType = {
   output_type: 'stream',
   name: 'stdout' | 'stderr',
   text: string,
 }
 
-export type ErrorOutput = {
+export type ErrorOutputModelType = {
   output_type: 'error',
   ename: string,
   evalue: string,
   traceback: Immutable.List<string>,
 }
 
-export type Output = ExecuteResult | DisplayData | StreamOutput | ErrorOutput;
+export type OutputModelType = ExecuteResultModelType | DisplayDataModelType | StreamOutputModelType | ErrorOutputModelType;
 
-export type CodeCell = {
+export type CodeCellModelType = {
   cell_type: 'code',
   metadata: ImmutableJSONMap,
   execution_count: ExecutionCount,
   source: string,
-  outputs: Immutable.List<Output>,
+  outputs: Immutable.List<OutputModelType>,
 }
 
-export type MarkdownCell = {
+export type MarkdownCellModelType = {
   cell_type: 'markdown',
   source: string,
   metadata: ImmutableJSONMap,
 }
 
-export type Cell = MarkdownCell | CodeCell;
+export type CellModelType = MarkdownCellModelType | CodeCellModelType;
 
-export type KernelspecMetadata = {
+export type KernelspecMetadataModelType = {
   name: string,
   display_name: string,
 }
 
-export type LanguageInfoMetadata = {
+export type LanguageInfoMetadataModelType = {
   name: string,
   codemirror_mode?: string | ImmutableJSONMap,
   file_extension?: string,
@@ -64,15 +67,15 @@ export type LanguageInfoMetadata = {
   pygments_lexer?: string,
 }
 
-export type NotebookMetadata = {
-  kernelspec: KernelspecMetadata,
-  language_info: LanguageInfoMetadata,
+export type NotebookMetadataModelType = {
+  kernelspec: KernelspecMetadataModelType,
+  language_info: LanguageInfoMetadataModelType,
 }
 
 export type Notebook = {
-  cellMap: Immutable.Map<string, Cell>,
+  cellMap: Immutable.Map<string, CellModelType>,
   cellOrder: Immutable.List<string>,
   nbformat: 4,
   nbformat_minor: 0 | 1 | 2 | 3 | 4,
-  metadata: NotebookMetadata,
+  metadata: NotebookMetadataModelType,
 }
